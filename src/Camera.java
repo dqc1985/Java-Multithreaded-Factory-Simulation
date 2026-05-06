05dc40c662313c1ad52febc1b6d774e6147a8324
dqc1985 <161266290+dqc1985@users.noreply.github.com>
2026-05-06 17:32:45 -0500

    Add files via upload

---
+++ src/Camera.java
@@ -0,0 +1,11 @@
+public class Camera implements Product {
+    @Override
+    public String getName() {
+        return "Camera";
+    }
+
+    @Override
+    public int getProductionTime() {
+        return 5;
+    }
+}
